1fda82073191df970c9ad5de80b5615d606ab2af
jdollar <dollarhidejoel@gmail.com>
2018-10-31 19:26:29 -0400

    Adding tests for blocktypes and furnace inventory for action additions

---
+++ src/test/java/net/glowstone/inventory/GlowFurnaceInventoryTest.java
@@ -0,0 +1,14 @@
+package net.glowstone.inventory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class GlowFurnaceInventoryTest {
+    private GlowFurnaceInventory glowFurnaceInventory;  
+
+    @Test
+    public void trueIsTrue() {
+        assertEquals(true, true);
+    }    
+}
